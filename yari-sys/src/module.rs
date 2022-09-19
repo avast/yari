fa0035bb9724095be2ad651103995c7661e2bd01
MatejKastak <matej.kastak@gmail.com>
2022-09-19 15:42:04 +0200

    feat(yari-sys): Add other known modules

--- yari-sys/src/module.rs
+++ yari-sys/src/module.rs
@@ -1,30 +1,41 @@
 use crate::error::YariError;
 use std::fmt::Display;
 
+/// Reference to a list of all currently active modules.
+pub static MODULES: &[Module] = &[
+    #[cfg(feature = "all_modules")]
+    Module::Androguard,
+    Module::Cuckoo,
+    Module::Dotnet,
+    Module::Elf,
+    Module::Hash,
+    Module::Magic,
+    #[cfg(feature = "all_modules")]
+    Module::Metadata,
+    Module::Math,
+    Module::Pe,
+    #[cfg(feature = "all_modules")]
+    Module::Phish,
+    Module::Time,
+];
+
 /// One of the supported YARA modules.
 #[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
 pub enum Module {
+    #[cfg(feature = "all_modules")]
+    Androguard,
     Cuckoo,
     Dotnet,
     Elf,
     Hash,
     Magic,
+    #[cfg(feature = "all_modules")]
+    Metadata,
     Math,
     Pe,
+    #[cfg(feature = "all_modules")]
+    Phish,
     Time,
-}
-
-impl Module {
-    pub const ALL_MODULES: [Self; 8] = [
-        Self::Cuckoo,
-        Self::Dotnet,
-        Self::Elf,
-        Self::Hash,
-        Self::Magic,
-        Self::Math,
-        Self::Pe,
-        Self::Time,
-    ];
 }
 
 impl std::str::FromStr for Module {
@@ -41,13 +52,19 @@ impl std::str::FromStr for Module {
     /// ```
     fn from_str(s: &str) -> std::result::Result<Self, YariError> {
         match s.to_lowercase().as_str() {
+            #[cfg(feature = "all_modules")]
+            "androguard" => Ok(Module::Androguard),
             "cuckoo" => Ok(Module::Cuckoo),
             "dotnet" => Ok(Module::Dotnet),
             "elf" => Ok(Module::Elf),
             "hash" => Ok(Module::Hash),
             "magic" => Ok(Module::Magic),
             "math" => Ok(Module::Math),
+            #[cfg(feature = "all_modules")]
+            "metadata" => Ok(Module::Metadata),
             "pe" => Ok(Module::Pe),
+            #[cfg(feature = "all_modules")]
+            "phish" => Ok(Module::Phish),
             "time" => Ok(Module::Time),
             _ => Err(YariError::UnknownModule(s.to_string())),
         }
@@ -57,13 +74,19 @@ impl std::str::FromStr for Module {
 impl AsRef<str> for Module {
     fn as_ref(&self) -> &str {
         match *self {
+            #[cfg(feature = "all_modules")]
+            Module::Androguard => "androguard",
             Module::Cuckoo => "cuckoo",
             Module::Dotnet => "dotnet",
             Module::Elf => "elf",
             Module::Hash => "hash",
             Module::Magic => "magic",
             Module::Math => "math",
+            #[cfg(feature = "all_modules")]
+            Module::Metadata => "metadata",
             Module::Pe => "pe",
+            #[cfg(feature = "all_modules")]
+            Module::Phish => "phish",
             Module::Time => "time",
         }
     }
